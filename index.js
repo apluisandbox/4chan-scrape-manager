f5174a606106604337e227a628952466b3ce4299
aplui <36481473+aplui@users.noreply.github.com>
2020-11-11 20:44:18 +0100

    work on localhost

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const ejs = require("ejs");
 app.set('view engine', 'ejs')
 app.use(express.static("public"));
 
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
 
-  res.render("home")
+res.render("home")
 })
 
 
 
 
-    app.listen(3000, function(req, res) {
-      console.log("Server started on port 3000.");
-    })
+app.listen(3000, function(req, res) {
+  console.log("Server started on port 3000.");
+})
