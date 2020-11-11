e38939e267c2933c5a499327d880ec1507ce0556
aplui <36481473+aplui@users.noreply.github.com>
2020-11-11 20:47:16 +0100

    bodyParser install

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
+const bodyParser = require("body-parser")
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
