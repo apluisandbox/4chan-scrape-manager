ee66828dd3a72857ecbb250fb93de53d468f4b40
aplui <36481473+aplui@users.noreply.github.com>
2020-11-11 20:30:04 +0100

    make modules and index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+//const chan = require("4chan");
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+
+app.set('view engine', 'ejs')
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+
+  res.render("home")
+})
+
+
+
+
+    app.listen(3000, function(req, res) {
+      console.log("Server started on port 3000.");
+    })
